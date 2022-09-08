969aaa1f41c3435f6878719005ca8fcf3dfcbfff
Adam Taras <ataras2@gmail.com>
2022-09-09 09:39:24 +1000

    added config test

--- tests/test_Prescription_Writer.py
+++ tests/test_Prescription_Writer.py
@@ -49,6 +49,17 @@ class Test_get_sw_line(unittest.TestCase):
         self.assertEqual(Prescription_Writer.get_sw_line(s,"tilt_x", False), '"text_tilt_x_2" = 0.000000')
         self.assertEqual(Prescription_Writer.get_sw_line(s,"tilt_y", False), '"text_tilt_y_2" = 45.000000')
         self.assertEqual(Prescription_Writer.get_sw_line(s,"tilt_z", False), '"text_tilt_z_2" = 0.000000')
+        
+    def test_ignoring_config(self):
+        # test using config number
+        s = Surface(0, np.array([0.1,0.,-0.4]), np.array([0.,45.,0.]), config=2, name='text')
+        
+        self.assertEqual(Prescription_Writer.get_sw_line(s,"x", False, use_config=False), '"text_x" = 0.100000')
+        self.assertEqual(Prescription_Writer.get_sw_line(s,"z", False, use_config=False), '"text_z" = -0.400000')
+        self.assertEqual(Prescription_Writer.get_sw_line(s,"y", False, use_config=False), '"text_y" = 0.000000')
+        self.assertEqual(Prescription_Writer.get_sw_line(s,"tilt_x", False, use_config=False), '"text_tilt_x" = 0.000000')
+        self.assertEqual(Prescription_Writer.get_sw_line(s,"tilt_y", False, use_config=False), '"text_tilt_y" = 45.000000')
+        self.assertEqual(Prescription_Writer.get_sw_line(s,"tilt_z", False, use_config=False), '"text_tilt_z" = 0.000000')
 
 
 if __name__ == '__main__':
