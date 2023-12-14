b8ab822e28e75cd90b92b7cb6f300505d10d6682
Adam Taras <ataras2@gmail.com>
2023-12-14 19:53:16 +1100

    simple test of cad string done

--- tests/test_surface.py
+++ tests/test_surface.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from zemax_to_cad.surface import Surface
+from zemax_to_cad.surface import Surface, StateSubset
 
 
 class TestSurface:
@@ -60,3 +60,17 @@ class TestSurface:
             np.array([1.0, 0.0, 0.5])
             + np.array([init_pos[1], init_pos[0], -init_pos[2]]),
         )
+
+    def test_cad_string(self):
+        s = Surface(
+            0,
+            np.array([0.1, 0.0, -0.4]),
+            np.array([0.0, 45.0, 0.0]),
+            name="text",
+        )
+
+        assert "0.1" in s.to_cad_string([StateSubset.X, StateSubset.Y])
+        assert "0.0" in s.to_cad_string([StateSubset.X, StateSubset.Y])
+        assert "text" in s.to_cad_string([StateSubset.X, StateSubset.Y])
+
+        assert "-0.4" not in s.to_cad_string([StateSubset.X, StateSubset.Y])
